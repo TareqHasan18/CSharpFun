cdd4f66eaff1da19de979f324d1a244331de380e
TareqHasan18 <42324208+TareqHasan18@users.noreply.github.com>
2019-10-24 20:53:13 -0400

    First commit

---
+++ CSharpFun/CSharpFun/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("This is all about C Sharp");
+        }
+    }
+}
