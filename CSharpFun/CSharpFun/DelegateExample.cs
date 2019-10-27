6ff276e13dc12621054c957766a297acb94dd91d
TareqHasan18 <42324208+TareqHasan18@users.noreply.github.com>
2019-10-27 11:38:21 -0400

    Added delegate example

---
+++ CSharpFun/CSharpFun/DelegateExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFun
+{
+    class DelegateExample
+    {
+        //A delegate is type safe function pointer. A delegate points to a function
+        //Signature of the delegate should match the signature of the function, that is why
+        // delegate is called type safe function pointer.
+        public static void Hello(string strMessage)
+        {
+            Console.WriteLine(strMessage);
+        }
+    }
+
+    public delegate void HelloFunctinDelegate(string Message);
+}
--- CSharpFun/CSharpFun/Program.cs
+++ CSharpFun/CSharpFun/Program.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace CSharpFun
 {
-    class Program
+    class Program : DelegateExample
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("This is all about C Sharp");
+            /*Console.WriteLine("This is all about C Sharp");
 
             Vehicle vehicle = new Vehicle();
             vehicle.Start();
@@ -27,7 +27,61 @@ namespace CSharpFun
             //vehicle.Luxary();
 
             Vehicle vehicle1 = new Car();
-            vehicle1.Luxary();
+            vehicle1.Luxary();*/
+
+
+            //Delegate Example
+            HelloFunctinDelegate del = new HelloFunctinDelegate(Hello);
+            del("Hello from Delegate");
+
+
+            //Delegate with another example
+            List<Employee> empList = new List<Employee>();
+
+            empList.Add(new Employee { ID = 101, Name = "Nancy", Salary = 5000, Experience = 5 });
+            empList.Add(new Employee { ID = 102, Name = "Mike", Salary = 4000, Experience = 4 });
+            empList.Add(new Employee { ID = 103, Name = "Smith", Salary = 6000, Experience = 6 });
+            empList.Add(new Employee { ID = 104, Name = "David", Salary = 3000, Experience = 3 });
+
+            //IsPromotable isPromotable = new IsPromotable(Promote);
+
+            Employee.PromoteEmployee(empList, /*isPromotable*/ emp => emp.Experience >= 5);
+        }
+
+        //when we use Lamda expression we don't need this function
+        /*public static bool Promote(Employee emp)
+        {
+            if(emp.Experience >= 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }*/
+    }
+
+    delegate bool IsPromotable(Employee empl);
+
+
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+
+        public static void PromoteEmployee(List<Employee> employeeList, IsPromotable IsEligibleToPromote)
+        {
+            foreach (Employee employee in employeeList)
+            {
+                if(IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine(employee.Name + " Promoted");
+                }
+            }
         }
     }
 }
